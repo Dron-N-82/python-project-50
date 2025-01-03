f6c710c8e02262d6ea2d40e014afbaa76452b513
Andrey Novikov <82.anovikov@gmail.com>
2025-01-04 00:22:32 +0300

    2 step of the project, modified gendiff.py

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -4,19 +4,17 @@ import argparse
 
 def main():
     # Создаем парсер аргументов
-    parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
-    
+    parser = argparse.ArgumentParser(
+        description='Compares two configuration files and shows a difference.',
+        epilog='set format of output')
     # Добавляем аргументы
     parser.add_argument('first_file', help='First file to compare.')
     parser.add_argument('second_file', help='Second file to compare.')
-    
+    parser.add_argument('-f', '--format')
     # Обрабатываем аргументы
     args = parser.parse_args()
     
-    # Здесь будет ваша логика сравнения (пока просто выводим аргументы)
-    # print('Сравниваем файлы:')
-    # print(f'  {args.first_file}')
-    # print(f'  {args.second_file}')
+    
 
 if __name__ == '__main__':
     main()
