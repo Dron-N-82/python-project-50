dce91be6049c7ff1e4b1684398b4f8f9b995f244
Andrey Novikov <82.anovikov@gmail.com>
2025-01-23 01:53:24 +0300

    6 step fix of the project: modified project.

--- tests/test_gendiff.py
+++
@@ -1,8 +0,0 @@
-from gendiff.generate_diff import maindiff
-
-def test_main(mocker):
-    mock = mocker.patch('gendiff.generate_diff.maindiff')
-    
-    from gendiff.scripts.gendiff import main
-    main()
-    mock.assert_called_once()
