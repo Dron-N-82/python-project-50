4eca040d46d2d658f503d829b4742555c0e49a9c
Andrey Novikov <82.anovikov@gmail.com>
2025-03-24 17:58:03 +0300

    Fix. End step9. End project!

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -1,12 +1,10 @@
 #!usr/bin/env python3
-#import argparse
 import gendiff.build_diff as diff_nested
 import gendiff.parse_file as p_f
 from gendiff.formatting import formating_diff
-#import gendiff.cli as cli
+
 
 def generate_diff(file1_path, file2_path, style='stylish'):
-    #arg_data = cli.parse_argumet()
     data1 = p_f.get_data_from_file(file1_path)
     data2 = p_f.get_data_from_file(file2_path)
     diff = diff_nested.generate_diff(data1, data2)
